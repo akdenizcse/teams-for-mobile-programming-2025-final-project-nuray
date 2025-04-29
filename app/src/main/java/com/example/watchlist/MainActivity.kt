439a695e8b9d9d3158770d30ca0d4cf666a8482b
NurayKoca <nuraykoca0738@gmail.com>
2025-04-29 19:28:39 +0300

    chance print state

--- app/src/main/java/com/example/watchlist/MainActivity.kt
+++ app/src/main/java/com/example/watchlist/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "This is a watchlist project for the lecture $name!",
         modifier = modifier
     )
 }
