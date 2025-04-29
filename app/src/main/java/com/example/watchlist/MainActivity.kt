6a6fe68814ce7865d180261df37c2d78514c9ce0
NurayKoca <nuraykoca0738@gmail.com>
2025-04-29 19:36:56 +0300

    First branch test

--- app/src/main/java/com/example/watchlist/MainActivity.kt
+++ app/src/main/java/com/example/watchlist/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "This is a watchlist project for the lecture $name!",
+        text = "Branch test $name!",
         modifier = modifier
     )
 }
